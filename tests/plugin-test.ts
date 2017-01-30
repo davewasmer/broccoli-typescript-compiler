d5c7e21141647cfd67532e5ea585b972c0f417e0
Kris Selden <kris.selden@gmail.com>
2017-01-30 12:50:55 -0800

    more wip

--- tests/plugin-test.ts
+++ tests/plugin-test.ts
@@ -1,61 +1,48 @@
 import "mocha";
-import * as fs from "fs-extra";
 import { expect } from "chai";
 import * as broccoli from "broccoli";
-import * as walkSync from "walk-sync";
 import * as path from "path";
 import * as fixturify from "fixturify";
+import * as os from "os";
+import * as mkdirp from "mkdirp";
+import * as rimraf from "rimraf";
 
 import filter = require("../lib/index");
 
-let expectations = 'tests/expectations';
-
-function entryFor(path, entries) {
-  for (let i = 0; i < entries.length; i++) {
-    if (entries[i].relativePath === path) {
-      return entries[i];
-    }
-  }
-}
-
-describe('transpile TypeScript', function() {
+describe("transpile TypeScript", function() {
   this.timeout(10000);
-  let builder;
 
-  // TODO: random tmpdir
-  var INPUT_PATH = path.resolve('tmp/input');
+  let INPUT_PATH = path.join(os.tmpdir(), "broccoli-typescript-compiler-test");
 
-  beforeEach(function() {
-    fs.mkdirpSync(INPUT_PATH);
-    fixturify.writeSync(INPUT_PATH, fixturify.readSync('tests/fixtures/files'));
+  beforeEach(done => {
+    mkdirp(INPUT_PATH, done);
+    fixturify.writeSync(INPUT_PATH, fixturify.readSync("tests/fixtures/files"));
   });
 
-  afterEach(function () {
-    fs.removeSync(INPUT_PATH);
-    return builder.cleanup();
+  afterEach(done => {
+    rimraf(INPUT_PATH, done);
   });
 
-  describe('tsconfig', function() {
-    it('uses tsconfig path from options', function () {
-      builder = new broccoli.Builder(filter(INPUT_PATH, {
-        tsconfig: 'tests/fixtures/tsconfig.json'
+  describe("tsconfig", function() {
+    it("uses tsconfig path from options", function () {
+      let builder = new broccoli.Builder(filter(INPUT_PATH, {
+        tsconfig: "tests/fixtures/tsconfig.json"
       }));
 
-      return builder.build().then(function(results) {
-        var outputPath = results.directory;
-        var entries = walkSync.entries(outputPath);
-
-        expect(entries).to.have.length(3);
-
-        var output = fs.readFileSync(outputPath + '/fixtures.js', 'UTF8');
-        var input = fs.readFileSync(expectations + '/expected.es6', 'UTF8');
-
-        expect(output).to.eql(input);
+      return builder.build().then(() => {
+        let actual = fixturify.readSync(builder.outputPath);
+        expect(actual).to.deep.equal({
+          "fixtures.js": "import Greeter from './types';\nclass Person {\n    constructor(name) {\n        this._name = name;\n    }\n    name() {\n        return this._name;\n    }\n}\ndocument.body.innerHTML = new Greeter().greet(new Person(\"Godfrey\"));\n",
+          "orange.js": "var orange : String;",
+          "types.js": "export default class Greeter {\n    greet(thing) {\n        return \"<h1>Hello, \" + thing.name() + \"</h1>\";\n    }\n}\n;\n;\n"
+        });
+      }).finally(() => {
+        builder.cleanup();
       });
     });
 
-    it('uses tsconfig json from options', function () {
-      builder = new broccoli.Builder(filter(INPUT_PATH, {
+    it("uses tsconfig json from options", () => {
+      let builder = new broccoli.Builder(filter(INPUT_PATH, {
         tsconfig: {
           "compilerOptions": {
             "target": "es2015",
@@ -65,148 +52,146 @@ describe('transpile TypeScript', function() {
           }
         }
       }));
-
-      return builder.build().then(function(results) {
-        var outputPath = results.directory;
-        var entries = walkSync.entries(outputPath);
-
-        expect(entries).to.have.length(3);
-
-        var output = fs.readFileSync(outputPath + '/fixtures.js', 'UTF8');
-        var input = fs.readFileSync(expectations + '/expected.es6', 'UTF8');
-
-        expect(output).to.eql(input);
+      return builder.build().then(() => {
+        let actual = fixturify.readSync(builder.outputPath);
+        expect(actual).to.deep.equal({
+          "fixtures.js": "import Greeter from './types';\nclass Person {\n    constructor(name) {\n        this._name = name;\n    }\n    name() {\n        return this._name;\n    }\n}\ndocument.body.innerHTML = new Greeter().greet(new Person(\"Godfrey\"));\n",
+          "orange.js": "var orange : String;",
+          "types.js": "export default class Greeter {\n    greet(thing) {\n        return \"<h1>Hello, \" + thing.name() + \"</h1>\";\n    }\n}\n;\n;\n"
+        });
+      }).finally(() => {
+        builder.cleanup();
       });
     });
 
-    describe('tsconfig resolution', function() {
-      it('basic resolution', function () {
-        // since this uses the project tsconfig I need these in lib
-        var Funnel = require('broccoli-funnel');
-        var input = new Funnel(INPUT_PATH, {
-          destDir: 'lib'
-        });
-        builder = new broccoli.Builder(filter(input));
+    // describe('tsconfig resolution', function() {
+    //   it('basic resolution', function () {
+    //     // since this uses the project tsconfig I need these in lib
+    //     var Funnel = require('broccoli-funnel');
+    //     var input = new Funnel(INPUT_PATH, {
+    //       destDir: 'lib'
+    //     });
+    //     builder = new broccoli.Builder(filter(input));
 
-        return builder.build().then(function(results) {
-          var outputPath = results.directory;
+    //     return builder.build().then(function(results) {
+    //       var outputPath = results.directory;
 
-          var actualJS = fs.readFileSync(outputPath + '/dist/fixtures.js').toString();
-          // var actualMap = fs.readFileSync(outputPath + '/dist/fixtures.js.map').toString();
-          var expectedJS = fs.readFileSync(expectations + '/expected.js').toString();
-          // var expectedMap = fs.readFileSync(expectations + '/expected.js.map').toString();
+    //       var actualJS = fs.readFileSync(outputPath + '/dist/fixtures.js').toString();
+    //       // var actualMap = fs.readFileSync(outputPath + '/dist/fixtures.js.map').toString();
+    //       var expectedJS = fs.readFileSync(expectations + '/expected.js').toString();
+    //       // var expectedMap = fs.readFileSync(expectations + '/expected.js.map').toString();
 
-          expect(actualJS).to.eql(expectedJS);
-          // expect(actualMap).to.eql(expectedMap);
-        });
-      });
-    });
+    //       expect(actualJS).to.eql(expectedJS);
+    //       // expect(actualMap).to.eql(expectedMap);
+    //     });
+    //   });
+    // });
   });
 
-  describe('rebuilds', function() {
-    var lastEntries, outputPath;
+  // describe('rebuilds', function() {
+  //   var lastEntries, outputPath;
 
-    beforeEach(function() {
-      builder = new broccoli.Builder(filter(INPUT_PATH, {
-        tsconfig: 'tests/fixtures/tsconfig.json'
-      }));
+  //   beforeEach(function() {
+  //     builder = new broccoli.Builder(filter(INPUT_PATH, {
+  //       tsconfig: 'tests/fixtures/tsconfig.json'
+  //     }));
 
-      return builder.build().then(function(results) {
-        outputPath = results.directory;
+  //     return builder.build().then(function(results) {
+  //       outputPath = results.directory;
 
-        lastEntries = walkSync.entries(outputPath);
-        expect(lastEntries).to.have.length(3);
-        expect(lastEntries.map(function(entry) { return entry.relativePath; })).to.deep.equals([
-          'fixtures.js',
-          'orange.js',
-          'types.js'
-        ]);
-      });
-    });
+  //       lastEntries = walkSync.entries(outputPath);
+  //       expect(lastEntries).to.have.length(3);
+  //       expect(lastEntries.map(function(entry) { return entry.relativePath; })).to.deep.equals([
+  //         'fixtures.js',
+  //         'orange.js',
+  //         'types.js'
+  //       ]);
+  //     });
+  //   });
 
-    it('noop rebuild', function() {
-      return builder.build().then(function(results) {
-        var entries = walkSync.entries(results.directory);
+    // it('noop rebuild', function() {
+    //   return builder.build().then(function(results) {
+    //     var entries = walkSync.entries(results.directory);
 
-        expect(entries).to.deep.equal(lastEntries);
-        expect(entries).to.have.length(3);
-      });
-    });
+    //     expect(entries).to.deep.equal(lastEntries);
+    //     expect(entries).to.have.length(3);
+    //   });
+    // });
 
-    it('mixed rebuild', function() {
-      fixturify.writeSync(INPUT_PATH, {
-        'apple.ts': 'var apple : String',
-        red: {
-          'one.ts': 'var one : String',
-        }
-      });
+    // it('mixed rebuild', function() {
+    //   fixturify.writeSync(INPUT_PATH, {
+    //     'apple.ts': 'var apple : String',
+    //     red: {
+    //       'one.ts': 'var one : String',
+    //     }
+    //   });
 
-      return builder.build().then(function(results) {
-        var entries = walkSync.entries(results.directory);
-        expect(entries.map(function(entry) { return entry.relativePath; })).to.deep.equals([
-          'apple.js',
-          'fixtures.js',
-          'orange.js',
-          'red/',
-          'red/one.js',
-          'types.js'
-        ]);
+    //   return builder.build().then(function(results) {
+    //     var entries = walkSync.entries(results.directory);
+    //     expect(entries.map(function(entry) { return entry.relativePath; })).to.deep.equals([
+    //       'apple.js',
+    //       'fixtures.js',
+    //       'orange.js',
+    //       'red/',
+    //       'red/one.js',
+    //       'types.js'
+    //     ]);
 
-        expect(entryFor('fixtures.js', entries)).to.eql(entryFor('fixtures.js', lastEntries));
-        expect(entryFor('types.js',    entries)).to.eql(entryFor('types.js',    lastEntries));
-        expect(entryFor('orange.js',   entries)).to.eql(entryFor('orange.js', lastEntries));
+    //     expect(entryFor('fixtures.js', entries)).to.eql(entryFor('fixtures.js', lastEntries));
+    //     expect(entryFor('types.js',    entries)).to.eql(entryFor('types.js',    lastEntries));
+    //     expect(entryFor('orange.js',   entries)).to.eql(entryFor('orange.js', lastEntries));
 
-        expect(entryFor('apple.js',   entries)).to.not.eql(entryFor('apple.js',   lastEntries));
-        expect(entryFor('red/',       entries)).to.not.eql(entryFor('red/',       lastEntries));
-        expect(entryFor('red/one.js', entries)).to.not.eql(entryFor('red/one.js', lastEntries));
+    //     expect(entryFor('apple.js',   entries)).to.not.eql(entryFor('apple.js',   lastEntries));
+    //     expect(entryFor('red/',       entries)).to.not.eql(entryFor('red/',       lastEntries));
+    //     expect(entryFor('red/one.js', entries)).to.not.eql(entryFor('red/one.js', lastEntries));
 
-        var update = fs.readFileSync('tests/fixtures/files/fixtures.ts', 'utf8');
+    //     var update = fs.readFileSync('tests/fixtures/files/fixtures.ts', 'utf8');
 
-        fixturify.writeSync(INPUT_PATH, {
-          'apple.ts': update
-        })
+    //     fixturify.writeSync(INPUT_PATH, {
+    //       'apple.ts': update
+    //     })
 
-        lastEntries = entries;
-        return builder.build();
-      }).then(function (results) {
-        var entries = walkSync.entries(results.directory);
-        expect(entryFor('fixtures.js', entries)).to.eql(entryFor('fixtures.js', lastEntries));
-        expect(entryFor('types.js',    entries)).to.eql(entryFor('types.js',    lastEntries));
-        expect(entryFor('orange.js',   entries)).to.eql(entryFor('orange.js', lastEntries));
-        expect(entryFor('red/',        entries)).to.eql(entryFor('red/', lastEntries));
-        expect(entryFor('red/one.js',  entries)).to.eql(entryFor('red/one.js', lastEntries));
+    //     lastEntries = entries;
+    //     return builder.build();
+    //   }).then(function (results) {
+    //     var entries = walkSync.entries(results.directory);
+    //     expect(entryFor('fixtures.js', entries)).to.eql(entryFor('fixtures.js', lastEntries));
+    //     expect(entryFor('types.js',    entries)).to.eql(entryFor('types.js',    lastEntries));
+    //     expect(entryFor('orange.js',   entries)).to.eql(entryFor('orange.js', lastEntries));
+    //     expect(entryFor('red/',        entries)).to.eql(entryFor('red/', lastEntries));
+    //     expect(entryFor('red/one.js',  entries)).to.eql(entryFor('red/one.js', lastEntries));
 
-        expect(entryFor('apple.js',    entries)).to.not.eql(entryFor('apple.js',   lastEntries));
+    //     expect(entryFor('apple.js',    entries)).to.not.eql(entryFor('apple.js',   lastEntries));
 
-        var expectedJS = fs.readFileSync(expectations + '/expected.es6').toString();
-        var actualJS = fs.readFileSync(outputPath + '/apple.js').toString();
+    //     var expectedJS = fs.readFileSync(expectations + '/expected.es6').toString();
+    //     var actualJS = fs.readFileSync(outputPath + '/apple.js').toString();
 
-        expect(actualJS).to.eql(expectedJS);
+    //     expect(actualJS).to.eql(expectedJS);
 
-        fixturify.writeSync(INPUT_PATH, {
-          'apple.ts': null,
-          red: null,
-        });
+    //     fixturify.writeSync(INPUT_PATH, {
+    //       'apple.ts': null,
+    //       red: null,
+    //     });
 
-        lastEntries = entries;
+    //     lastEntries = entries;
 
-        return builder.build();
-      }).then(function(results) {
-        var entries = walkSync.entries(results.directory);
-        expect(entries).to.not.deep.equal(lastEntries);
-        expect(entries).to.have.length(3);
+    //     return builder.build();
+    //   }).then(function(results) {
+    //     var entries = walkSync.entries(results.directory);
+    //     expect(entries).to.not.deep.equal(lastEntries);
+    //     expect(entries).to.have.length(3);
 
-        // expected stability
-        expect(entryFor('fixtures.js', entries)).to.eql(entryFor('fixtures.js', lastEntries));
-        expect(entryFor('types.js',    entries)).to.eql(entryFor('types.js', lastEntries));
-        expect(entryFor('orange.js',    entries)).to.eql(entryFor('orange.js', lastEntries));
+    //     // expected stability
+    //     expect(entryFor('fixtures.js', entries)).to.eql(entryFor('fixtures.js', lastEntries));
+    //     expect(entryFor('types.js',    entries)).to.eql(entryFor('types.js', lastEntries));
+    //     expect(entryFor('orange.js',    entries)).to.eql(entryFor('orange.js', lastEntries));
 
-        expect(entries.map(function(entry) { return entry.relativePath; })).to.deep.equals([
-          'fixtures.js',
-          'orange.js',
-          'types.js'
-        ]);
-      });
-    });
-  });
+    //     expect(entries.map(function(entry) { return entry.relativePath; })).to.deep.equals([
+    //       'fixtures.js',
+    //       'orange.js',
+    //       'types.js'
+    //     ]);
+    //   });
+    // });
+  // });
 });
